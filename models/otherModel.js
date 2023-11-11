a2c23c05a429da6e31dbd902ad28eb08cb45bf83
rahul <rahul.baradol.14@gmail.com>
2023-11-12 00:34:22 +0530

    minor change

--- models/otherModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const otherSchema = new mongoose.Schema({
-    aboutme: {type: String, required: true}
-}, {timestamps: true})
-
-export default mongoose.models.other || mongoose.model("other", otherSchema, "other")
