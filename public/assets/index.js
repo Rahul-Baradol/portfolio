8bcc3816ed42ba99722acf9015dc5ab429da7b68
rahul <rahul.baradol.14@gmail.com>
2024-03-08 15:55:00 +0530

    tech

--- public/assets/index.js
+++ public/assets/index.js
@@ -8,8 +8,30 @@ import linkedin from './social/linkedin2.png'
 import leetcode from './social/leetcode.png'
 import experience from './experience/quality.png'
 import CognitiveLabCertificate from './Certificates/CognitiveLab.png'
+import arduino from './tech/arduino.png'
+import aws from './tech/aws.png'
+import c from './tech/c.png'
+import cpp from './tech/cpp.png'
+import css from './tech/css.png'
+import docker from './tech/docker.png'
+import express from './tech/express.png'
+import go from './tech/go.png'
+import javascript from './tech/javascript.png'
+import mongodb from './tech/mongodb.svg'
+import nextjs from './tech/nextjs.png'
+import nginx from './tech/nginx.svg'
+import nodejs from './tech/nodejs.png'
+import python from './tech/python.png'
+import react from './tech/react.png'
+import selenium from './tech/selenium.png'
+import tailwindcss from './tech/tailwindcss.png'
+import typescript from './tech/typescript.png'
+import firebase from './tech/firebase.png'
+import linux from './tech/linux.png'
+import asm from './tech/asm.png'
 
 export {
+  asm,
   logoBlackR,
   menu,
   close,
@@ -18,5 +40,25 @@ export {
   linkedin,
   leetcode,
   experience,
-  CognitiveLabCertificate
+  CognitiveLabCertificate,
+  arduino,
+  aws,
+  c,
+  cpp,
+  css,
+  docker,
+  express,
+  go,
+  javascript,
+  mongodb,
+  nextjs,
+  nginx,
+  nodejs,
+  python,
+  react,
+  selenium,
+  tailwindcss,
+  typescript,
+  firebase,
+  linux
 };
