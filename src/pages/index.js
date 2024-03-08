b31a77a663e294ea59c12c75f6b6fda75dadcf68
rahul <rahul.baradol.14@gmail.com>
2024-03-08 16:16:06 +0530

    nextjs image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,44 +3,24 @@ import dynamic from 'next/dynamic';
 import { servicesOffered, projects, otherData } from '../../constants/constants'
 import { Divider, Spinner } from '@nextui-org/react';
 import Footer from '@/components/Footer';
+import HomeRecipe from '../components/HomeRecipe';
+import WorkRecipe from '../components/WorkRecipe';
 
 require('dotenv').config({ path: '.env.local' });
 
 export default function Home(props) {
-   const [HomeCompo, setHomeCompo] = useState(null);
-   const [WorkRecipe, setWorkRecipe] = useState(null);
-
-   useEffect(() => {
-      setHomeCompo(() => {
-         return dynamic(() => import('../components/HomeRecipe'), {
-            ssr: false,
-            loading: () => <div className='w-screen h-screen flex justify-center items-center'>
-               <Spinner color="secondary" />
-            </div>
-         });
-      })
-   }, [setHomeCompo]);
-
-   useEffect(() => {
-      setWorkRecipe(() => {
-         return dynamic(() => import('../components/WorkRecipe'), {
-            ssr: false,
-            loading: () => <div className='w-screen h-screen flex justify-center items-center'>
-               <Spinner color="secondary" />
-            </div>
-         })
-      })
-   }, [setWorkRecipe])
-
    return (
       <>
-         {
+         {/* {
             HomeCompo ? <HomeCompo aboutme={props.otherData.aboutme} /> : <></>
          }
 
          {
             WorkRecipe ? <WorkRecipe servicesOffered={props.servicesOffered} projects={props.projects} /> : <></>
-         }
+         } */}
+
+         <HomeRecipe aboutme={props.otherData.aboutme} />
+         <WorkRecipe servicesOffered={props.servicesOffered} projects={props.projects} />
 
          <Footer />
       </>
