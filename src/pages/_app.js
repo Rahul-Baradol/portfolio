5aa971c637d43b23295dd3665285446ef0a242a0
rahul <rahul.baradol.14@gmail.com>
2024-03-08 18:13:25 +0530

    dev quotes in intro

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,26 +1,33 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import Head from 'next/head';
 import DockerIntro from "@/components/Intro";
-import LoadingBar from "react-top-loading-bar";
-import { useRouter } from "next/router";
+// import LoadingBar from "react-top-loading-bar";
+// import { useRouter } from "next/router";
+import { devQuotes } from "../../constants/constants";
+
+function generateRandomNumber(a, b) {
+    // Generate random number between a and b (inclusive)
+    return Math.floor(Math.random() * (b - a + 1)) + a;
+}
 
 export default function App({ Component, pageProps }) {
   const [intro, setIntro] = useState(true);
-  const [progress, setProgress] = useState(0);
-  const router = useRouter();
+  // const [progress, setProgress] = useState(0);
 
-  useEffect(() => {
-    router.events.on("routeChangeStart", () => {
-      setProgress(40);
-    })
+  // const router = useRouter();
 
-    router.events.on("routeChangeComplete", () => {
-      setProgress(100);
-    })
-  }, [router.query, router.events])
+  // useEffect(() => {
+  //   router.events.on("routeChangeStart", () => {
+  //     setProgress(40);
+  //   })
+
+  //   router.events.on("routeChangeComplete", () => {
+  //     setProgress(100);
+  //   })
+  // }, [router.query, router.events])
 
   return (
     <>
@@ -46,17 +53,17 @@ export default function App({ Component, pageProps }) {
       </style>
 
       <NextUIProvider>
-        <LoadingBar 
+        {/* <LoadingBar 
           color="#2d2159"
           waitingTime={800}
           height={4}
           progress={progress} 
           onLoaderFinished={() => setProgress(0)} 
-        />
+        /> */}
         <div className="parent">
           <div className="child">
             <div className="relative z-0">
-              {intro ? <DockerIntro setIntro={setIntro} />
+              {intro ? <DockerIntro quoteId={generateRandomNumber(0, devQuotes.length-1)} setIntro={setIntro} />
                 : <>
                     <Navbar />
                     <Component {...pageProps} />
