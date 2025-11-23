3b23772cd6fbfeaf7a30974c2659a44ad68a6cc4
Rahul Baradol <rahul.baradol.14@gmail.com>
2025-11-23 13:32:20 +0530

    feat: experience container

---
+++ src/components/experience-card.tsx
@@ -0,0 +1,198 @@
+import { ArrowDown, Github, Globe, Linkedin } from "lucide-react";
+import { motion } from "motion/react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import * as AccordionPrimitive from "@radix-ui/react-accordion"
+import { TechBadge } from "./tech-badges";
+
+interface ExperienceCardProps {
+    companyName: string;
+    companyLogo: string;
+    websiteUrl: string;
+    githubUrl?: string;
+    linkedinUrl?: string;
+    role: string;
+    techBadges: {
+        redirect_url: string;
+        image_url: string;
+        tech: string;
+        size?: number;
+    }[];
+    bulletPoints?: string[];
+    timeline: string;
+    active?: boolean;
+}
+
+export default function ExperienceCard({ companyName, companyLogo, websiteUrl, githubUrl, role, linkedinUrl, timeline, active, techBadges, bulletPoints }: ExperienceCardProps) {
+    return (
+        <AccordionPrimitive.Root
+            data-slot="accordion"
+            type="single"
+            collapsible
+        >
+            <AccordionPrimitive.Item data-slot="accordion-item" value="item-1" className="flex flex-col gap-2">
+                <AccordionPrimitive.Header className="flex">
+                    <AccordionPrimitive.Trigger
+                        data-slot="accordion-trigger"
+                        className="focus-visible:border-ring focus-visible:ring-ring/50 flex-1 gap-4 p-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180 group w-full cursor-pointer border border-white/10 rounded-xl bg-black hover:bg-cyan-600/10 hover:border-cyan-500/5"
+                    >
+                        <div className="flex flex-col items-center w-full">
+                            <motion.div
+                                variants={{
+                                    hidden: { opacity: 0, y: 30 },
+                                    show: { opacity: 1, y: 0 },
+                                }}
+                                transition={{ duration: 0.5 }}
+                                className="flex flex-row justify-between items-center overflow-hidden w-full"
+                            >
+                                <div className="w-full sm:w-fit flex flex-row items-center gap-3">
+                                    <img
+                                        src={companyLogo}
+                                        width={45}
+                                    />
+
+                                    <div className="flex flex-col gap-1.5 sm:gap-0">
+                                        <div className="flex flex-col sm:flex-row sm:items-center gap-1 sm:gap-2">
+                                            <div className="text-sm italic">
+                                                {companyName}
+                                            </div>
+
+                                            <div className="flex flex-row gap-2 items-center">
+                                                <Tooltip>
+                                                    <TooltipTrigger asChild className="bg-black">
+                                                        <a href={websiteUrl} target="_blank" rel="noopener noreferrer">
+                                                            <Globe className="h-3 w-3 text-gray-400" />
+                                                        </a>
+                                                    </TooltipTrigger>
+                                                    <TooltipContent className="bg-black text-gray-400">
+                                                        Go to website
+                                                    </TooltipContent>
+                                                </Tooltip>
+
+                                                {
+                                                    githubUrl ? (
+                                                        <Tooltip>
+                                                            <TooltipTrigger asChild className="bg-black">
+                                                                <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+                                                                    <Github className="h-3 w-3 text-gray-400" />
+                                                                </a>
+                                                            </TooltipTrigger>
+                                                            <TooltipContent className="bg-black text-gray-400">
+                                                                Go to GitHub
+                                                            </TooltipContent>
+                                                        </Tooltip>
+                                                    ) : <></>
+                                                }
+
+                                                {
+                                                    linkedinUrl ? (
+                                                        <Tooltip>
+                                                            <TooltipTrigger asChild className="bg-black">
+                                                                <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+                                                                    <Linkedin className="h-3 w-3 text-gray-400" />
+                                                                </a>
+                                                            </TooltipTrigger>
+                                                            <TooltipContent className="bg-black text-gray-400">
+                                                                Go to LinkedIn
+                                                            </TooltipContent>
+                                                        </Tooltip>
+                                                    ) : <></>
+                                                }
+
+                                                {(active === true) ? <span className="w-2 h-2 bg-green-600/80 rounded-xl"></span> : <></>}
+                                            </div>
+                                        </div>
+
+                                        <div className="text-xs italic">
+                                            {role}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="text-xs italic text-gray-400 flex flex-row items-center gap-1 sm:gap-2">
+                                    <div>{timeline}</div> <ArrowDown className="w-4 sm:w-3" />
+                                </div>
+                            </motion.div>
+                        </div>
+                    </AccordionPrimitive.Trigger>
+                </AccordionPrimitive.Header>
+                <AccordionPrimitive.Content
+                    data-slot="accordion-content"
+                    className={`data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm `}
+                >
+                    <div className="p-4 border border-white/10 bg-black/5 rounded-xl flex flex-row items-center gap-1 flex-wrap">
+                        {
+                            techBadges.map((badge, index) => (
+                                <TechBadge
+                                    key={index}
+                                    redirect_url={badge.redirect_url}
+                                    image_url={badge.image_url}
+                                    tech={badge.tech}
+                                    size={badge.size}
+                                />
+                            ))
+                        }
+
+                        {
+                            bulletPoints && bulletPoints.length > 0 ? (
+                                <ul className="list-disc list-inside mt-2 space-y-1 text-gray-300">
+                                    {bulletPoints.map((point, index) => (
+                                        <li key={index}>{point}</li>
+                                    ))}
+                                </ul>
+                            ) : null
+                        }
+                    </div>
+                </AccordionPrimitive.Content>
+            </AccordionPrimitive.Item>
+        </AccordionPrimitive.Root>
+    );
+}
+
+interface ExperienceContainerProps {
+    snaps: ExperienceCardProps[];
+}
+
+export function ExperienceContainer({ snaps }: ExperienceContainerProps) {
+    return (
+        <motion.div
+            variants={{
+                hidden: { opacity: 0, y: 20 },
+                show: {
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                        duration: 0.7,
+                        staggerChildren: 0.5,
+                    },
+                },
+            }}
+            initial="hidden"
+            animate="show"
+            className="pt-1 pb-0 text-gray-400 w-[90vw] lg:w-[50vw] h-fit rounded-xl relative"
+        >
+            <div className="pt-3  pb-4 relative z-10 bg-black rounded-xl h-full flex flex-col gap-5">
+                <div className="flex flex-row items-center italic">
+                    Experience
+                </div>
+
+                {
+                    snaps.map((snap: ExperienceCardProps, index: number) => (
+                        <ExperienceCard
+                            key={index}
+                            companyName={snap.companyName}
+                            companyLogo={snap.companyLogo}
+                            websiteUrl={snap.websiteUrl}
+                            githubUrl={snap.githubUrl}
+                            linkedinUrl={snap.linkedinUrl}
+                            techBadges={snap.techBadges}
+                            bulletPoints={snap.bulletPoints}
+                            role={snap.role}
+                            timeline={snap.timeline}
+                            active={snap.active}
+                        />
+                    ))
+                }
+            </div>
+        </motion.div>
+    );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import { SnipSnapContainer } from "@/components/snip-snap";
 import { SpotifyCard } from "@/components/spotify-card";
 import { motion } from "motion/react";
 import { TechBadge } from "@/components/tech-badges";
+import { ExperienceContainer } from "@/components/experience-card";
 
 export function Home() {
     return (
@@ -23,7 +24,7 @@ export function Home() {
                 className=" w-[90vw] lg:w-[50vw] text-lg md:text-xl text-muted-foreground flex flex-col gap-1 items-center"
             >
                 <blockquote className="text-sm md:text-base italic text-gray-400 flex flex-row flex-wrap items-center justify-center gap-2">
-                    <div className="text-xs sm:text-sm">" crafting fast, reliable, and scalable web apps - using</div>
+                    <div className="text-xs sm:text-sm">crafting fast, reliable, and scalable web apps — using</div>
 
                     <TechBadge redirect_url="https://www.typescriptlang.org/" image_url="/ts.png" tech="Typescript" />
                     <TechBadge redirect_url="https://reactjs.org/" image_url="/react.png" tech="React" />
@@ -31,13 +32,75 @@ export function Home() {
                     <TechBadge redirect_url="https://fastapi.tiangolo.com/" image_url="/fastapi.png" tech="FastAPI" />
                     <TechBadge redirect_url="https://golang.org/" image_url="/go.png" tech="Go" size={16} /> <div>and</div>
                     <TechBadge redirect_url="https://nodejs.org/" image_url="/node.png" tech="Node.js" size={20} />
-                    <div className="text-xs sm:text-sm">"</div>
+                    {/* <div className="text-xs sm:text-sm">"</div> */}
                 </blockquote>
             </motion.div>
 
             <SpotifyCard />
 
-            {/* <TechCard /> */}
+            <ExperienceContainer 
+                snaps={[
+                    {
+                        companyName: "Alaiy",
+                        companyLogo: "/alaiy.png",
+                        websiteUrl: "https://alaiy.com",
+                        timeline: "July 2025 - Present",
+                        linkedinUrl: "https://www.linkedin.com/company/alaiy/posts/?feedView=all",
+                        techBadges: [
+                            { redirect_url: "https://www.typescriptlang.org/", image_url: "/ts.png", tech: "Typescript" },
+                            { redirect_url: "https://reactjs.org/", image_url: "/react.png", tech: "React" },
+                            { redirect_url: "https://fastapi.tiangolo.com/", image_url: "/fastapi.png", tech: "FastAPI" }
+                        ],
+                        bulletPoints: [
+                            "Developed and maintained scalable web applications using TypeScript, React, and FastAPI, enhancing user experience and performance.",
+                            "Collaborated with cross-functional teams to design and implement new features, resulting in a 20% increase in user engagement.",
+                            "Optimized backend services for improved response times, reducing latency by 30% through efficient database queries and caching strategies."
+                        ],
+                        role: "Full stack",
+                        active: true
+                    },
+                    {
+                        companyName: "CRED",
+                        companyLogo: "/cred.png",
+                        websiteUrl: "https://cred.club",
+                        timeline: "July 2024 - June 2025",
+                        linkedinUrl: "https://www.linkedin.com/company/credapp/",
+                        techBadges: [
+                            { redirect_url: "https://www.java.com/en/", image_url: "/java.png", tech: "Java" },
+                            { redirect_url: "https://spring.io/projects/spring-boot/", image_url: "/springboot.png", tech: "Spring Boot" },
+                            { redirect_url: "https://www.python.org/", image_url: "/python.png", tech: "Python" },
+                            { redirect_url: "https://golang.org/", image_url: "/go.png", tech: "Go", size: 17 },
+                            { redirect_url: "https://www.mysql.com/", image_url: "/mysql.png", tech: "MySQL" },
+                        ],
+                        bulletPoints: [
+                            "Improved performance by identifying and eliminating redundant database writes, reducing write IOPS by 87% (from 1600 to 200 ops/sec) and lowering CPU utilization from 70% to 20%.",
+                            "Revived a critical data ingestion pipeline for a major bank, achieving 99.9% user coverage",
+                            "Diagnosed a bug and restored a bank's data extraction funnel, fixing 300K workflows."
+                        ],
+                        role: "Backend Developer Intern",
+                        active: false
+                    },
+                    {
+                        companyName: "CognitiveLab",
+                        companyLogo: "/cognitivelab.png",
+                        websiteUrl: "https://www.cognitivelab.in/",
+                        timeline: "April 2024 - May 2024",
+                        linkedinUrl: "https://www.linkedin.com/company/cognitivelabai/",
+                        techBadges: [
+                            { redirect_url: "https://www.typescriptlang.org/", image_url: "/ts.png", tech: "Typescript" },
+                            { redirect_url: "https://reactjs.org/", image_url: "/react.png", tech: "React" },
+                            { redirect_url: "https://fastapi.tiangolo.com/", image_url: "/fastapi.png", tech: "FastAPI" }
+                        ],
+                        bulletPoints: [
+                            "Owned end-to-end development of the chat platform, designed to handle medical science queries",
+                            "Led the development of user-facing web application using Nextjs, Tailwind CSS and ShadCN, ensuring responsive and accessible UI",
+                            "Built the FastAPI-based backend, enabling structured query handling and real-time responses."
+                        ],
+                        role: "Full stack",
+                        active: false
+                    }
+                ]}
+            />
 
             <SnipSnapContainer
                 snaps={[
