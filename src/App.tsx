095dc99e941eb44c5ee096e7156817f7b70f3045
Rahul Baradol <rahul.baradol.14@gmail.com>
2026-05-11 21:58:58 +0530

    fix: hydration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { lazy, Suspense, useEffect } from "react";
+import { useEffect } from "react";
 import Lenis from "lenis";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./layout";
 import { ThemeProvider } from "./lib/theme";
-
-const Home = lazy(() => import("./pages/home").then((m) => ({ default: m.Home })));
-const PRsPage = lazy(() => import("./pages/prs").then((m) => ({ default: m.PRsPage })));
-const NotFound = lazy(() => import("./components/not-found"));
+import { Home } from "./pages/home";
+import { PRsPage } from "./pages/prs";
+import NotFound from "./components/not-found";
 
 function App() {
   useEffect(() => {
@@ -23,15 +22,13 @@ function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <Suspense>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="/prs" element={<PRsPage />} />
-              <Route path="/*" element={<NotFound />} />
-            </Route>
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/prs" element={<PRsPage />} />
+            <Route path="/*" element={<NotFound />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot, hydrateRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { PostHogProvider } from 'posthog-js/react'
+import posthog from 'posthog-js'
 
-const options = {
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: '2025-05-24',
-} as const
+})
 
 const rootEl = document.getElementById('root')!
 const app = (
   <StrictMode>
-    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
-      <App />
-    </PostHogProvider>
+    <App />
   </StrictMode>
 )
 
